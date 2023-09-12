57e63824fd58a03eab5164fab2137171d0f5333e
drew <Drew Malone>
2023-09-12 16:13:10 -0400

    older os version

--- vars.tf
+++ vars.tf
@@ -16,5 +16,6 @@ variable "ssh_key" {
 variable "workstation_ami_id" {
   type    = string
   #default = "ami-0001f433d458dcfce"  # Ubuntu 22.10 in us-east-2
-  default = "ami-04c27040eb48fd96f"  # Ubuntu 20.04 in us-west-2
+  #default = "ami-04c27040eb48fd96f"  # Ubuntu 20.04 in us-west-2
+  default = "ami-04c27040eb48fd96f"  # Ubuntu 16.04 in us-west-2
 }
